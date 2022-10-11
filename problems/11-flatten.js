be5638aaf633380c2ae5c637a6aaf267cb3d9a5b
Nick <bassnlasky@gmail.com>
2022-10-11 15:21:34 -0700

    problem 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -13,9 +13,60 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here
+  if(arr.length === 0){return arr}
+  let newArr = []
+  for(let i = 0; i < arr.length; i++ ){
+    let el = arr[i]
+    if(typeof el === 'number'){
+      newArr.push(el)
+    }else {
+      newArr.push(...flatten(el))
+    }
+  }
+  return newArr
 }
-  
+
+// function flatten(arr) {
+//   if(arr.length === 0){return arr}
+//   let newArr = []
+//   for(let i = 0; i < arr.length; i++ ){
+//     let el = arr[i]
+//     if(Array.isArray(el)){
+//       newArr.push(flatten(el))
+//     }else {
+//       newArr.push(el)
+//     }
+//   }
+//   return newArr
+// }
+
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3])
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function flatten(arr) {
+//   if(arr.length === 0){return arr}
+//   let newArr = []
+//   for(let i = 0; i < arr.length; i++ ){
+//     let el = arr[i]
+//     if(Array.isArray(el)){
+//       newArr.push(flatten(el))
+//     }else {
+//       newArr.push(el)
+//     }
+//   }
+//   return newArr
+// }
